6d64ae8e8eff509984d8670d20573b0433bd4d29
Pedro Lucas <zdtplankito@gmail.com>
2024-07-27 02:08:04 -0300

    Up to date

--- nextjs-frontend/pages/_document.js
+++ nextjs-frontend/pages/_document.js
@@ -4,7 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="../app/favicon.ico" />
+
+        <link rel="icon" href="../app/generic-logo-icon.ico" />
+
         <link rel="stylesheet" type='text/css' href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"/>
       </Head>
       <body>
